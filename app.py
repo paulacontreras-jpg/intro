8f76f5b9f33fc38c1abc8d9a928a93cf9fb8b098
paulacontreras-jpg <mpcontrert@eafit.edu.co>
2026-08-20 10:04:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ from PIL import Image
 st.title("HOLA")
 st.header("En este espacio voy a poner mi mood de hoy")
 st.write("hoy me siento asi:")
-image = image.open("#healing.jfif")
+image = image.open(´#healing.jfif´)
 st.image(image,captions="sisoy")
