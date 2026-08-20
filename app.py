10407a1c43e892661849baf2bfa3edc88dfca69c
paulacontreras-jpg <mpcontrert@eafit.edu.co>
2026-08-20 10:18:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,3 +9,20 @@ st.image(image, caption='hola')
 
 texto= st.text_input("Escribe tu mood","Este es tu mood")
 st.write('Tu mood es',texto)
+
+st.subheader("Ahora pondre dos culumnas")
+
+col1, col2 =stcolumns(2)
+
+with col1:
+  st.subheader("Esta es la primera columna")
+  st.write("Todos los dias me siento con el mismo mood")
+  resp=st.checkbox('Me pasa igual')
+  if resp:
+    st.write('TWINS')
+
+with col2:
+  st.subheader("Esta es la segunda columna")
+  modo =st.checkbox("Los gatos te ayudan a manejar tu mood?")
+  if modo == 'visual':
+    st.write(`TWINS X2`)
